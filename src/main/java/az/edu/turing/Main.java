06b2c34d3ab60264323ccee3efa1eff95a9b0595
Ruslan19-05 <nusrtcrkzov@gmail.com>
2024-10-26 17:59:19 +0400

    riyazi emeller

---
+++ src/main/java/az/edu/turing/Main.java
@@ -0,0 +1,26 @@
+package az.edu.turing;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int number1 = 15;
+        int number2 = 7;
+        int sum;
+        sum = number1 + number2;
+        System.out.println(sum);
+        int subt;
+        subt = number1 - number2;
+        System.out.println(subt);
+        int divde;
+        divde = number1 / number2;
+        System.out.println(divde);
+        int mult;
+        mult = number1 * number2;
+        System.out.println(mult);
+        int divde2;
+        divde2 = number1 % number2;
+        System.out.println(divde2);
+
+
+    }}
